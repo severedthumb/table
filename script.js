e9c14cf624729f8f301697908b957cacd3949f0a
Jeremy <severed.thumb.studio@gmail.com>
2024-08-21 06:07:36 -0500

    Added HP up/down button functionality.

--- script.js
+++ script.js
@@ -6,21 +6,21 @@ const hammerSelect = document.getElementById('HammerSelect');
 const harbeckSelect = document.getElementById('HarbeckSelect');
 const waldorSelect = document.getElementById('WaldorSelect');
 
-
-
 const test = document.getElementById('test');
 const test2 = document.getElementById('test2');
 const headshot = document.querySelector('.headshot')
 const currentHP = document.getElementById('currentHP');
 const maxHP = document.getElementById('maxHP');
 
+
+/* THIS INFORMATION ABOUT PCS IS HERE FOR TESTING PURPOSES. LATER THIS WILL BE DRAWN FROM A DATABASE. */
 const cade = {
     'name': 'Cade',
     'race': 'human',
     'class': 'rogue',
     'headshot': './assets/Cade_Headshot.jpg',
-    'currentHP': 12,
-    'maxHP': 48
+    'currentHP': 0,
+    'maxHP': 0
 };
 
 const hammer = {
@@ -28,8 +28,8 @@ const hammer = {
     'race': 'dwarf',
     'class': 'fighter',
     'headshot': './assets/Hammer_Headshot.jpg',
-    'currentHP': 51,
-    'maxHP': 61
+    'currentHP': 0,
+    'maxHP': 0
 };
 
 const harbeck = {
@@ -37,8 +37,8 @@ const harbeck = {
     'race': 'dwarf',
     'class': 'cleric',
     'headshot': './assets/Harbeck_Headshot.jpg',
-    'currentHP': 44,
-    'maxHP': 44
+    'currentHP': 0,
+    'maxHP': 0
 };
 
 const waldor = {
@@ -46,12 +46,13 @@ const waldor = {
     'race': 'elf',
     'class': 'wizard',
     'headshot': './assets/Waldor_Headshot.jpg',
-    'currentHP': 12,
-    'maxHP': 13
+    'currentHP': 0,
+    'maxHP': 0
 };
 
 
 
+/* THIS FUNCTION RUNS WHEN A PC IS SELECTED, AND FILLS IN THE INFORMATION */
 fillInfoBox = (character) => {
     headshot.setAttribute('src', character.headshot);
     test.innerHTML = character.name;
@@ -66,21 +67,56 @@ fillInfoBox = (character) => {
 
 
 
+/* THESE FUNCTIONS SELECT THE PC. THIS NEEDS TO BE REFACTORED. */
+
+let selectedPC;
+
 cadeSelect.addEventListener('click', () => {
+    selectedPC = cade;
     fillInfoBox(cade);
 });
 
 hammerSelect.addEventListener('click', () => {
+    selectedPC = hammer;
     fillInfoBox(hammer);
 });
 
 harbeckSelect.addEventListener('click', () => {
+    selectedPC = harbeck;
     fillInfoBox(harbeck);
 });
 
 waldorSelect.addEventListener('click', () => {
+    selectedPC = waldor;
     fillInfoBox(waldor);
 });
 
 
 
+/* THESE FOUR FUNCTION HANDLE THE HP UP/DOWN BUTTONS */
+/* These are written in such a way that current HP cannot go above the max HP, and max HP cannot go below current HP */
+document.querySelector('.currentHP_up').addEventListener('click', () => {
+    if (selectedPC.currentHP < selectedPC.maxHP) {
+        selectedPC.currentHP += 1;
+        currentHP.innerHTML = selectedPC.currentHP;
+    };
+});
+
+document.querySelector('.currentHP_down').addEventListener('click', () => {
+    if (selectedPC.currentHP > 0) {
+        selectedPC.currentHP -= 1;
+        currentHP.innerHTML = selectedPC.currentHP;
+    };
+});
+
+document.querySelector('.maxHP_up').addEventListener('click', () => {
+    selectedPC.maxHP += 1;
+    maxHP.innerHTML = selectedPC.maxHP;
+});
+
+document.querySelector('.maxHP_down').addEventListener('click', () => {
+    if (selectedPC.maxHP > 0 && selectedPC.maxHP > selectedPC.currentHP) {
+        selectedPC.maxHP -= 1;
+        maxHP.innerHTML = selectedPC.maxHP;
+    };
+});
